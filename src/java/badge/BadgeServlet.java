c60c4875d3f28fc5c5e2d88cef78fd33760e51c8
cwm24 <cwm24@ITARGET1096.univ.pitt.edu>
2015-02-03 17:22:49 -0500

    Added Parameters.java and updated servlet

--- src/java/badge/BadgeServlet.java
+++ src/java/badge/BadgeServlet.java
@@ -8,10 +8,12 @@ package badge;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -31,18 +33,49 @@ public class BadgeServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet BadgeServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet BadgeServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        
+        Parameters p = new Parameters();
+        
+        String name = request.getParameter("name");
+        String textcolors = request.getParameter("textcolors");
+        String bgcolors = request.getParameter("bgcolors");
+        String shape = request.getParameter("shape");
+        String x = request.getParameter("x");
+        String y = request.getParameter("y");
+        String fstyle = request.getParameter("fstyle");
+        String ftsize = request.getParameter("ftsize");
+        
+        p.setName(name);
+        p.setBgcolor(bgcolors);
+        p.setColorString(textcolors);
+        p.setShape(shape);
+        p.setStringX(x);
+        p.setStringY(y);
+        p.setFtstyle(fstyle);
+        p.setFtsizeString(ftsize);
+        
+//        try (PrintWriter out = response.getWriter()) {
+//            /* TODO output your page here. You may use following sample code. */
+//            out.println("<!DOCTYPE html>");
+//            out.println("<html>");
+//            out.println("<head>");
+//            out.println("<title>Servlet BadgeServlet</title>");            
+//            out.println("</head>");
+//            out.println("<body>");
+//            out.println("<p> name: " + p.getName()+ "</p>");
+//            out.println("<p> bgcolor: " + p.getBgcolor()+ "</p>");
+//            out.println("<p> shape: " + p.getShape()+ "</p>");
+//            out.println("<p> ftsizeString: " + p.getFtsizeString()+ "</p>");
+//            out.println("</body>");
+//            out.println("</html>");
+//        }
+        
+        HttpSession session = request.getSession();
+        session.setAttribute("Parameters", p);
+
+        // forward the request (not redirect)
+        RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");;
+        dispatcher.forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
